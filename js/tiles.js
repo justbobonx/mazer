09236c6ac709a956e2dd7fa5c31e06f3770884e2
just bob <317064714+justbobonx@users.noreply.github.com>
2026-09-09 15:30:09 -0400

    Reload a wall type on cycle if its images never actually loaded

--- js/tiles.js
+++ js/tiles.js
@@ -4,47 +4,57 @@ function TileSet(types) {
   this.types = types && types.length ? types.slice() : [1];
   this.index = 0;
   this.sets = {};
-  this.ready = false;
 }
 
 TileSet.prototype.currentType = function () {
   return this.types[this.index];
 };
 
-TileSet.prototype.cycle = function () {
-  if (!this.types.length) return this.currentType();
-  this.index = (this.index + 1) % this.types.length;
-  return this.currentType();
+TileSet.prototype.srcFor = function (type, mask) {
+  return "images/wall_" + type + "_" + mask + ".png";
 };
 
-TileSet.prototype.load = function () {
-  const self = this;
-  let left = this.types.length * 16;
-  this.ready = false;
-  this.sets = {};
-  for (let t = 0; t < this.types.length; t++) {
-    const type = this.types[t];
-    const images = new Array(16);
-    this.sets[type] = images;
-    for (let i = 0; i < 16; i++) {
-      const img = new Image();
-      img.onload = function () {
-        left -= 1;
-        if (left <= 0) self.ready = true;
-      };
-      img.onerror = function () {
-        left -= 1;
-        if (left <= 0) self.ready = true;
-      };
-      img.src = "images/wall_" + type + "_" + i + ".png";
-      images[i] = img;
-    }
+TileSet.prototype.typeReady = function (type) {
+  const images = this.sets[type];
+  if (!images) return false;
+  for (let i = 0; i < 16; i++) {
+    const img = images[i];
+    if (!img || !img.complete || !img.naturalWidth) return false;
+  }
+  return true;
+};
+
+TileSet.prototype.load = function (type) {
+  if (type == null) {
+    this.sets = {};
+    for (let t = 0; t < this.types.length; t++) this.load(this.types[t]);
+    return;
+  }
+  const images = new Array(16);
+  this.sets[type] = images;
+  for (let i = 0; i < 16; i++) {
+    const img = new Image();
+    img.src = this.srcFor(type, i);
+    images[i] = img;
   }
 };
 
+TileSet.prototype.cycle = function () {
+  if (!this.types.length) return this.currentType();
+  this.index = (this.index + 1) % this.types.length;
+  const type = this.currentType();
+  if (!this.typeReady(type)) this.load(type);
+  return type;
+};
+
 TileSet.prototype.draw = function (ctx, maze, originX, originY, cellSize) {
-  const images = this.sets[this.currentType()];
-  if (!images) return;
+  let type = this.currentType();
+  if (!this.typeReady(type)) {
+    this.load(type);
+    type = this.types.find((t) => this.typeReady(t));
+    if (type == null) return;
+  }
+  const images = this.sets[type];
   for (let y = 0; y < maze.rows; y++) {
     for (let x = 0; x < maze.cols; x++) {
       const mask = maze.cell(y, x).mask();
